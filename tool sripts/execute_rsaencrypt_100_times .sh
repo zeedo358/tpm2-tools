a3add384934af63f0e1be52271662f30dc95d0e1
zeedo358 <zeedo358@gmail.com>
2021-12-17 14:12:18 +0200

    Add comments, and delete sudo prefix before commands

--- tool sripts/execute_rsaencrypt_100_times .sh
+++ tool sripts/execute_rsaencrypt_100_times .sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
+# this script requires root permissions
 TEST_FILES=('8bytes.txt' '16bytes.txt' '32bytes.txt' '64bytes.txt' '128bytes.txt' '200bytes.txt' '245bytes.txt')
 
 for test_file in "${TEST_FILES[@]}"
@@ -6,7 +7,7 @@ do
     echo "File: $test_file"
     for (( i = 1; i <= 100; i++ ))
     do
-        sudo tpm2_rsaencrypt -c key.ctx -o msg.enc $test_file > output.txt 2>&1
+        tpm2_rsaencrypt -c key.ctx -o msg.enc $test_file > output.txt 2>&1
         cat output.txt | grep "INFO: Elapsed time (ms):" | awk '{print $5}'
     done
 done
