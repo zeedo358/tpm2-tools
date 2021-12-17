84333b3a1b4cd043bf22e121f57c3390a71bbdd1
zeedo358 <zeedo358@gmail.com>
2021-12-17 12:56:00 +0200

    Add bash scripts to initialize tpm and to execute encryption for RSA algorithm

---
+++ tool sripts/execute_rsaencrypt_100_times .sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+TEST_FILES=('8bytes.txt' '16bytes.txt' '32bytes.txt' '64bytes.txt' '128bytes.txt' '200bytes.txt' '245bytes.txt')
+
+for test_file in "${TEST_FILES[@]}"
+do
+    echo "File: $test_file"
+    for (( i = 1; i <= 100; i++ ))
+    do
+        sudo tpm2_rsaencrypt -c key.ctx -o msg.enc $test_file > output.txt 2>&1
+        cat output.txt | grep "INFO: Elapsed time (ms):" | awk '{print $5}'
+    done
+done
